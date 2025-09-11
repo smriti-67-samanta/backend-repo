69540dd20b81ee4440f1e40646296c55ffa0b27e
smriti-67-samanta <samantasmriti0@gmail.com>
2025-09-11 21:04:08 +0530

    Add files via upload

---
+++ user-address-system/models/Address.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const addressSchema = new mongoose.Schema({
+  street: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    default: 'India'
+  },
+  pincode: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Address', addressSchema);
---
+++ user-address-system/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  age: {
+    type: Number,
+    min: 0
+  },
+  addresses: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Address'
+  }]
+});
+
+module.exports = mongoose.model('User', userSchema);
